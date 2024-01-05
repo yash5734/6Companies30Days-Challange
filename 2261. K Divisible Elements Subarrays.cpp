727761afb731b165eea1156c26274610c97c17fd
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2024-01-05 22:22:48 +0530

    Create 2261. K Divisible Elements Subarrays.cpp

---
+++ 2261. K Divisible Elements Subarrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countDistinct(vector<int>& nums, int k, int p) {
+        set<vector<int>> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            vector<int> ds;
+            int temp = 0;
+            for (int j = i; j < nums.size(); j++) {
+                ds.push_back(nums[j]);
+                if (nums[j] % p==0) {
+                    temp++;
+                }
+                if(temp>k) break;
+                if (temp <= k) ans.insert(ds);
+            }
+        }
+
+        return ans.size();
+    }
+};
