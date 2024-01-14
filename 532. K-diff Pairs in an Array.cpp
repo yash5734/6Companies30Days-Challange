dd931bc2bf27245831fe376b9c4e7a2495b6f23c
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2024-01-14 16:37:13 +0530

    Create 532. K-diff Pairs in an Array.cpp

---
+++ 532. K-diff Pairs in an Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+        int ans =0;
+        for(auto it:mp){
+            if(k==0){
+                if(it.second>1){
+                    ans++;
+                }
+            }else{
+                if(mp.find(it.first + k)!=mp.end()){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
